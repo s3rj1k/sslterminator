4f99d2dcbcc955cc4e5d116fc1b5e257750ccbd4
s3rj1k <evasive.gyron@gmail.com>
2020-01-12 19:14:15 +0200

    WiP
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

---
+++ cli/go.mod
@@ -0,0 +1,9 @@
+module test
+
+go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/garyburd/redigo v1.6.0
+	github.com/gomodule/redigo v2.0.0+incompatible
+)
---
+++ cli/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/gob"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+const (
+	dbAddress = "127.0.0.1:6379"
+)
+
+func main() {
+	db := &redis.Pool{
+		MaxIdle:   2,
+		MaxActive: 20,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial(
+				"tcp",
+				dbAddress,
+				redis.DialDatabase(15),
+			)
+		},
+	}
+
+	crt, err := tls.LoadX509KeyPair(os.Args[1], os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(crt.Certificate) < 1 {
+		log.Fatal(1)
+	}
+
+	crt.Leaf, err = x509.ParseCertificate(crt.Certificate[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := make(map[string]struct{})
+
+	m[strings.TrimPrefix(crt.Leaf.Subject.CommonName, "*")] = struct{}{}
+	for i := range crt.Leaf.DNSNames {
+		m[strings.TrimPrefix(crt.Leaf.DNSNames[i], "*")] = struct{}{}
+	}
+
+	gob.RegisterName("rsa.PublicKey", rsa.PublicKey{})
+	gob.RegisterName("rsa.PrivateKey", rsa.PrivateKey{})
+
+	var b bytes.Buffer
+	enc := gob.NewEncoder(&b)
+
+	if err := enc.Encode(crt); err != nil {
+		log.Fatal(err)
+	}
+
+	c := db.Get()
+	defer c.Close()
+
+	for k := range m {
+		if err = c.Send("SET", k, b); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// var decCrt tls.Certificate
+
+	// dec := gob.NewDecoder(&b)
+	// err = dec.Decode(&decCrt)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// spew.Dump(m, b, decCrt)
+}
